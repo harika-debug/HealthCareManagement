b90882ba59d711088d2282845ba5bf9913812e0e
Dell <Dell@DESKTOP-23R7NNS>
2020-04-17 17:22:47 +0530

    first commit-1

---
+++ HealthCareManagement/src/main/java/com/cg/HealthCareManagement/App.java
@@ -0,0 +1,15 @@
+package com.cg.HealthCareManagement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+   
+    System.out.println("springBoot Backend app");
+    }
+}
